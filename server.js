8b1eb90686dcd6d6255dd933a85af0ec4087cefd
sonu <sonushilla189@gmail.com>
2024-08-28 18:29:43 +0530

    first commit

--- server.js
+++ server.js
@@ -1,62 +1,76 @@
 import express from "express";
 import axios from "axios";
 import pkg from 'pg';
+
 const { Client } = pkg;
-let localdata=[];
-let Database=false;
+let localdata = [];
+let Database = false;
+
 const db = new Client({
-    user: "postgres", // Corrected key name from 'username' to 'user'
+    user: "postgres",
     database: "Crypto",
-    port: 5432, // Port should be an integer, not a string
+    port: 5432,
     host: "localhost",
     password: "Sonu@123"
 });
 
-try {
-    // Establish connection
-    db.connect();
-    Database=true;
-    console.log("Connected to the database successfully");
-} catch (err) {
-    console.error("Error connecting to the database:", err.stack);
-} 
-const app=express();
+async function init() {
+    try {
+        // Establish connection
+        await db.connect();
+        Database = true;
+        console.log("Connected to the database successfully");
+    } catch (err) {
+        console.error("Error connecting to the database:", err.stack);
+    }
 
+    try {
+        const response = await axios.get("https://api.wazirx.com/api/v2/tickers");
+        const dataArray = Object.values(response.data);
 
-    try{
-    const response=await axios.get("https://api.wazirx.com/api/v2/tickers");
-
-    const dataArray = Object.values(response.data);
-    let i;
-    for(i=0;i<10;i++)
-    {   if(Database)
-    {
-            await db.query("delete from crypto");
-            await db.query(`insert into crypto (name,last,buy,sell,volume,base_unit) Values ($1,$2,$3,$4,$5,$6)`,[dataArray[i].name,dataArray[i].last,dataArray[i].buy,dataArray[i].sell,dataArray[i].volume,dataArray[i].base_unit]);
-            console.log("data added successfully");}
-            else{
-             const data={name:dataArray[i].name,
-            last:dataArray[i].last,
-            buy: dataArray[i].buy
-            ,sell:dataArray[i].sell
-            ,volume:dataArray[i].volume
-            ,base_unit:dataArray[i].base_unit}
-            localdata.push(data);}
+        for (let i = 0; i < 10; i++) {
+            if (Database) {
+                await db.query("DELETE FROM crypto");
+                await db.query(
+                    `INSERT INTO crypto (name, last, buy, sell, volume, base_unit) VALUES ($1, $2, $3, $4, $5, $6)`,
+                    [dataArray[i].name, dataArray[i].last, dataArray[i].buy, dataArray[i].sell, dataArray[i].volume, dataArray[i].base_unit]
+                );
+                console.log("Data added successfully");
+            } else {
+                const data = {
+                    name: dataArray[i].name,
+                    last: dataArray[i].last,
+                    buy: dataArray[i].buy,
+                    sell: dataArray[i].sell,
+                    volume: dataArray[i].volume,
+                    base_unit: dataArray[i].base_unit
+                };
+                localdata.push(data);
+            }
         }
+    } catch (error) {
+        console.error("Error fetching data from the API:", error.message);
     }
-    catch(error)
-    {   res.sendStatus(500);
-        console.log(error);
-    }
+}
 
-app.get("/getdata", async (req,res)=>{
-if(Database){
-    const response = await db.query("select * from crypto");
-    res.send(response);
-}
-else{
-    res.status(200).json({ rows: localdata });
-    console.log(localdata);
-}
+const app = express();
+
+app.get("/getdata", async (req, res) => {
+    if (Database) {
+        try {
+            const response = await db.query("SELECT * FROM crypto");
+            res.status(200).json({ rows: response.rows });
+        } catch (error) {
+            console.error("Error querying the database:", error.message);
+            res.status(500).send("Error querying the database");
+        }
+    } else {
+        res.status(200).json({ rows: localdata });
+        console.log(localdata);
+    }
 });
-app.listen(5000,()=>{console.log("server running on port 5000");})
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+    init(); // Initialize database and fetch data
+});
